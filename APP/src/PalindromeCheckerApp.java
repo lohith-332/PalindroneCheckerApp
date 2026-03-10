dcab3c6bd6ff10129060d32578a7e30384756aff
lohith-332 <lohithsairaj@gmail.com>
2026-03-10 09:38:26 +0530

    lohith added UC-1

---
+++ APP/src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+// version 1.0
+// author Abc
+// useCase 1: Welcome page
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker App Management System");
+    }
+
+}
